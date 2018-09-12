fe325392adf790a002f409f47bb8b403ad92192f
Steeper62 <42715767+Steeper62@users.noreply.github.com>
2018-09-12 07:07:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$string = "indianaJonesAndTheLastCrusade";
+$booleen = false;
+$integer = 1989;
+$float = 8.3;
+
+echo $string; ?>
+<br>
+<?php if($booleen == false)
+    echo "False"; ?>
+<br>
+<?php echo $integer; ?>
+<br>
+<?php echo $float; ?>
+
+
+
